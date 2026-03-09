685729484eca725e294b4ebe5165259e3bdc99e9
Paolo Bejarano <p.bejaranosamayani@alum.up.edu.pe>
2026-03-09 18:51:28 -0500

    Return note id when created

--- core/views.py
+++ core/views.py
@@ -143,6 +143,16 @@ class NoteCreateView(generics.CreateAPIView):
     
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        
+        # Return the full note data including ID
+        note = serializer.instance
+        note_serializer = NoteSerializer(note)
+        return Response(note_serializer.data, status=status.HTTP_201_CREATED)
 
 
 @extend_schema(
